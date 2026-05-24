c4021b8f98b8be47f47cf33b65a90818c9e1422b
Sri-Mahi <chadalavadasrimahi@gmail.com>
2026-05-24 21:38:18 +0530

    Add allowedHosts configuration for Vite server

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,9 @@ export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
+  vite: {
+       server: {
+       allowedHosts: ["ayugenixz-biosolutions.onrender.com"],
+       },
+  },
 });
